2a891e6279bc95853eb3e11e4f381ecac9c73720
C1F5D2DD <3238858647@qq.com>
2026-05-04 17:18:48 +0800

    更新插件

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from astrbot.api.all import *
-from astrbot.api.event import filter, AstrMessageEvent
-import logging
-
-logger = logging.getLogger(__name__)
+from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
+from astrbot.api.star import Context, Star, register
+from astrbot.api import logger # 使用 astrbot 提供的 logger 接口
+import astrbot.api.message_components as Comp
+from astrbot.api import AstrBotConfig
+from astrbot.api.provider import ProviderRequest
 
 @register("astrbot_plugin_astrbot_dev_plg", "c1f5d2dd", "收到任何消息时打印消息链", "v1.0")
 class MessageLoggerPlugin(Star):
