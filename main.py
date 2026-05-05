88ebe97b4ebf5053de26454f1d4aff8534e35b52
C1F5D2DD <3238858647@qq.com>
2026-05-05 16:59:32 +0800

    b

--- main.py
+++ main.py
@@ -17,13 +17,13 @@ class MessageLoggerPlugin(Star):
     async def on_message(self, event: AstrMessageEvent):
         """收到任何消息时，用 logger 打印消息链"""
         try:
-            message_chain =  event.message_obj.message
+            message =  event.get_messages()
         except AttributeError:
             logger.info("空消息链")
             return
 
         logger.info(f"收到消息链: \n")
-        for msg in message_chain:
-            logging.info(msg)
+        for msg in message:
+            logging.info(type(msg))
         # 不阻断消息，让其他插件继续处理
         return
